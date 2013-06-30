592a8aa0e7f829d0e072e9ff2cfc1fb4bd37e0d6
Liam Kirsher <liamk@numenet.com>
2013-06-30 00:53:32 -0700

    Debugging

--- chef/cookbooks/mongodb/definitions/mongodb.rb
+++ chef/cookbooks/mongodb/definitions/mongodb.rb
@@ -206,12 +206,15 @@ define :mongodb_instance, :mongodb_type => "mongod" , :action => [:enable, :star
     p ">>>>>>>>>>>>>>>>> replicaset: #{replicaset.nil?}"
     ruby_block "config_replicaset" do
       block do
-        if not replicaset_name.nil?
+        p ">>>>>>>>>>>>>>>>> replicaset: #{replicaset}"
+        if not replicaset.nil?
           p ">>>>>>>>>>>>>>>>> replicaset not nil: #{replicaset} #{replicaset_name} #{rs_nodes}"
           if not node[:mongodb][:use_ebs_snapshots]
             p ">>>>>>>>>>>>>>>>> configuring: #{replicaset} #{replicaset_name} #{rs_nodes}"
             MongoDB.configure_replicaset(replicaset, replicaset_name, rs_nodes)
           end
+	else
+          p ">>>>>>>>>>>>>>>>> replicaset: #{replicaset}"
         end
       end
       action :nothing
